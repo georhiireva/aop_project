0021a87a16dea45c03fea50afc2e631ebb8fd454
Georgii Reva <gosha.dopustim@gmail.com>
2021-01-20 23:52:24 +0300

    Update README.md. Add simple sampe of JoinPoint for AfterReturningAdvice

--- src/main/java/aop/aspect/AfterReturningAspect.java
+++ src/main/java/aop/aspect/AfterReturningAspect.java
@@ -1,15 +1,26 @@
 package aop.aspect;
 
 import model.impl.Book;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * выполняется только после нормального окончания метода с основной логикой
  */
 @Component
 @Aspect
+/**
+ * выполняется только после нормального окончания метода с основной логикой,
+ * но до присвоения результата этого метода какой-либо переменной.
+ * Поэтому с помощью @AfterReturning Advice возможно изменять возвращаемый результат метод
+ */
 public class AfterReturningAspect {
     @AfterReturning(pointcut = "!aop.poincut.Pointcuts.allVoidMethods()", returning = "retVal")
     public Object changeReturningValueAfterReturnBookMethod(Object retVal) {
@@ -21,5 +32,14 @@ public class AfterReturningAspect {
             return book;
         }
         return retVal;
+    }
+
+    @AfterReturning(value = "execution(public model.impl.Book getBook(String)))", returning = "retVal")
+    public void afterReturningGetBookMethod(JoinPoint joinPoint, Book retVal) {
+        System.out.println("afterReturningGetBookMethod: демонстрация работы JoinPoint");
+        System.out.println("afterReturningGetBookMethod: сигнатура метода: " + joinPoint.getSignature());
+        System.out.println(
+                "afterReturningGetBookMethod: аргументы метода: " +
+                        Arrays.stream(joinPoint.getArgs()).reduce((x, y) -> x + ", " + y).get());
     }
 }
